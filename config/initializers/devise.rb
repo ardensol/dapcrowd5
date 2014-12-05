6ae6a7aafe4c0844491bf813cfa14721eb157b18
carlos <csolorio05@gmail.com>
2014-12-04 19:39:04 -0500

    updated fb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,9 +229,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
 
-  config.omniauth :facebook, "762916483795753", "9b7ecb41905e53e26d0a67109b116615"
+  config.omniauth :facebook, "762916483795753", "9b7ecb41905e53e26d0a67109b116615",  :secure_image_url => true
 
-  secure_image_url: true
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
